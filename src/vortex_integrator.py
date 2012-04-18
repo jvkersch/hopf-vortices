91ca216dc74f196b2ac069ff5140c35e068a197f
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-04-18 16:52:38 -0700

    Optimization routines (crude version) for FPI

--- src/vortex_integrator.py
+++ src/vortex_integrator.py
@@ -1,21 +1,42 @@
 import numpy as np
+from vectors import row_product
 from su2_geometry import cayley_klein
+from continuous_vortex_system import scaled_gradient_hamiltonian
 
 class VortexSystem:
     h = 1e-1
+    N = 2
+    gamma = np.ones(2) 
+    sigma = 0.
 
-def optimization_direct(B, X0, vortex_system):
+def optimization_direct(b, x0, vortex_system):
     
-    A = vortex_system.h*B
-    U = cayley_klein(A)
-    X1 = np.einsum('abc, bdc -> adc', U, X0)
+    a = vortex_system.h*b
+    x1 = cayley_klein(a, x0)
+    
+    gradH = scaled_gradient_hamiltonian(
+        vortex_system.gamma, 
+        (x0+x1)/2., 
+        vortex_system.sigma)
+
+    dot = np.sum(x0*gradH, axis=1)
+
+    return 1./4*np.cross(np.cross(gradH, x0, axis=1) + row_product(dot, a), 
+                         x0, axis=1)
+                         
+def res(b, x0, vortex_system):
+    a = vortex_system.h*b
+    r = optimization_direct(a, x0, vortex_system)
+
+    return 2*row_product(vortex_system.gamma, b - r)
+
 
 def optimization_adjoint():
-    pass 
+    pass
 
 
 if __name__ == '__main__':
-    A = np.array([[2, 1, 3], [4, 6, 6]], dtype=np.double)
+    b = np.array([[2, 1, 3], [4, 6, 6]], dtype=np.double)
     v = VortexSystem()
     
-    print optimization_direct(A, 2*A, v)
+    print res(b, 2*b, v)
