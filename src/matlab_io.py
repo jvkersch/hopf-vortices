7bc6c2c00719894da6f5e5abd45f6fc7351b207b
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-04-18 16:52:56 -0700

    Matlab IO routines

---
+++ src/matlab_io.py
@@ -0,0 +1,24 @@
+import scipy.io as sio 
+
+
+def load_ic_from_matfile(filename):
+    c = sio.loadmat(filename)
+
+    X0 = c['X0']
+    gamma = c['gamma'].reshape(X0.shape[1])
+
+    return gamma, X0
+
+
+def save_ic_to_matfile(filename, gamma, X0):
+    sio.savemat(filename, {'X0': X0, 'gamma': gamma}, oned_as='column')
+
+
+def load_variables_from_matfile(filename, varnames):
+    c = sio.loadmat(filename)
+    return [c[name] for name in varnames]
+
+
+def save_variables_to_matfile(filename, vardict):
+    sio.savemat(filename, vardict, oned_as='column')
+
