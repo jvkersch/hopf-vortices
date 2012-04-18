529529a75b245e207b2dfc8bf1bc52d9742e88a9
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-04-18 06:29:54 -0700

    Generic Cayley map

--- src/lie_algebra.py
+++ src/lie_algebra.py
@@ -1,31 +1,52 @@
 import numpy as np
 
-from vectors import vectorize
+def cayley(A):
+    r"""
+    Apply Cayley map to elements of a matrix Lie algebra. 
 
+    Parameters
+    ----------
 
-def cayley(xi):
-    """Applies Cayley map to the Lie algebra element xi.
+    A : array-like
+        Nxmxm array whose slices along the 0-axis are Lie algebra elements.
 
-    INPUT: 
+    Returns
+    -------
 
-      -- ``xi`` - Lie algebra matrix.
+    M : array-like
+        Nxmxm array whose slices are Lie group elements.
 
-    OUTPUT:
+    Examples
+    --------
 
-    Group element.
+    >>> from su2_geometry import hatmap
+    >>> A = hatmap([1, 2, 3]); A
+    array([[[ 0.+3.j,  2.+1.j],
+            [-2.+1.j, -0.-3.j]]])
+    >>> cayley(A)
+    array([[[-0.86666667+0.4j       ,  0.26666667+0.13333333j],
+            [-0.26666667+0.13333333j, -0.86666667-0.4j       ]]])
+
+    This Cayley map agrees with the `cayley_klein` for matrices in 
+    :math:`\mathfrak{su}(2)`:
+
+    >>> from su2_geometry import cayley_klein
+    >>> cayley_klein([1, 2, 3])
+    array([[[-0.86666667+0.4j       ,  0.26666667+0.13333333j],
+            [-0.26666667+0.13333333j, -0.86666667-0.4j       ]]])
 
     """
 
-    if xi.ndim == 2:
-        xi = np.array([xi])
+    A = np.array(A)
+    if A.ndim == 2:
+        A = np.array([A])
 
-    xi = np.array(xi)
-    m, _, n = xi.shape
+    N, m, _ = A.shape
     I = np.eye(m)
-    M = np.empty((m, m, n), dtype=complex)
+    M = np.empty((N, m, m), dtype=A.dtype)
 
-    for k in xrange(0, n):
-        element = xi[:, :, k]
-        M[:, :, k] = np.dot(element + I, np.linalg.inv(element - I))
+    for k in xrange(0, N):
+        element = A[k, :, :]
+        M[k, :, :] = np.dot(I + element, np.linalg.inv(I - element))
 
     return M
--- src/tests/cayley_test.py
+++ src/tests/cayley_test.py
@@ -4,20 +4,35 @@ sys.path.append('..')
 import unittest
 import numpy as np
 
-from vectors import random_array                    
-from su2_geometry import hatmap, is_SU
+from vectors import random_array, is_float_equal                  
+from su2_geometry import hatmap, is_SU, cayley_klein
 from lie_algebra import cayley
+
 
 class testCayley(unittest.TestCase):
 
     def test_su_to_SU(self):
+        """Test that the image of generic Cayley map ends up in SU(2)."""
 
         N = 16
         a = random_array((N, 3))
         A = hatmap(a)
         U = cayley(A)
 
-        self.assertTrue(is_SU(U))
+        self.assertTrue(is_SU(U).all())
+
+
+    def testCayleyKlein(self):
+        """Test whether generic Cayley and Cayley-Klein agree on su(2)."""
+
+        N = 16
+        a = random_array((N, 3))
+        A = hatmap(a)
+     
+        U1 = cayley(A)
+        U2 = cayley_klein(a)
+
+        self.assertTrue(is_float_equal(U1, U2))
 
 
 if __name__ == '__main__':
