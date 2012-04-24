021a0595294ebdf22fb3a404eb170656476aebf1
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-04-24 14:50:04 -0700

    Dedicated class for nonlinear solver which takes matrix arguments

---
+++ src/array_solver.py
@@ -0,0 +1,44 @@
+import scipy.optimize as so
+import numpy as np
+
+class FSolveArray:
+    """
+    Solve nonlinear equation where variables are 2D numpy arrays.
+
+    Examples
+    --------
+
+    >>> def fun(x0, A):
+    ...     return A*x0 - np.cos(x0)
+    ...
+    >>> x0 = np.array([[1., 0.5], [.3, .6]])
+    >>> A  = np.array([[1, 3], [2, 1]])
+    >>> f = FSolveArray(fun, size=(2,2))
+    >>> f.fsolve(x0, args=A, xtol=1e-5)
+    array([[ 0.7390852 ,  0.31675081],
+           [ 0.4501836 ,  0.73908516]])
+
+    """
+
+    def __init__(self, fun, size):
+
+        n, m = size
+        self.size = (n, m)
+        self.output_size = n*m
+
+        def fun_flattened(x0, *params):
+            x0.shape = self.size 
+            res = fun(x0, *params)
+            res.shape = self.output_size
+            return res
+
+        self.fun_flattened = fun_flattened
+
+    def fsolve(self, x0, **kwargs):
+        x0 = np.array(x0)
+        x0.shape = self.output_size
+
+        res = so.fsolve(self.fun_flattened, x0, **kwargs)
+        res.shape = self.size
+        return res
+
