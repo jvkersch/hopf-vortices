cf17efafe42275781a5519fabb5b02f254776537
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-04-24 16:35:32 -0700

    Bug in load_initial_conditions

--- src/matlab_io.py
+++ src/matlab_io.py
@@ -1,19 +1,23 @@
 import scipy.io as sio 
 
 
-def load_ic_from_matfile(filename):
+def load_initial_conditions(filename):
     c = sio.loadmat(filename)
 
-    sigma = c['sigma']
+    sigma = 0
+    if 'sigma' in c:
+        sigma = c['sigma']
+
     X0 = c['X0']
-    gamma = c['gamma'].reshape(X0.shape[1])
+    gamma = c['gamma'].reshape(X0.shape[0])
 
     sigma = sigma[0][0]
     return gamma, X0, sigma
 
 
-def save_ic_to_matfile(filename, gamma, X0):
-    sio.savemat(filename, {'X0': X0, 'gamma': gamma}, oned_as='column')
+def save_initial_conditions(filename, gamma, X0, sigma=0.0):
+    sio.savemat(filename, {'X0': X0, 'gamma': gamma, 'sigma' : sigma}, 
+                oned_as='column')
 
 
 def load_variables_from_matfile(filename, varnames):
