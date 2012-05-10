48a5fecaee2273efbb53d1e0fc00dc9f56e93181
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-05-10 23:17:13 +0200

    Simulation of generic vortex initial conditions.

---
+++ simulations/generic/generic_simulation.py
@@ -0,0 +1,12 @@
+from simulation import Simulation
+
+# Sphere integrator
+s = Simulation()
+s.load_initial_conditions('random_initial_conditions.mat')
+s.run_simulation(h=.1, sim='sphere', tmax=1000)
+s.post_process()
+s.save_results('random_h01.mat')
+
+s.run_simulation(h=.01, sim='sphere', tmax=1000)
+s.post_process()
+s.save_results('random_h001.mat')
