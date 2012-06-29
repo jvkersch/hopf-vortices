0e427bf463d50a1327811c4e500498769bebb02e
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-06-28 18:03:08 -0700

    Comparison with other integrators (generic vortices)

--- simulations/generic/generic_simulation.py
+++ simulations/generic/generic_simulation.py
@@ -3,10 +3,10 @@ from hopf.simulation import Simulation
 # Sphere integrator
 s = Simulation()
 s.load_initial_conditions('random_initial_conditions.mat')
-s.run_simulation(h=.1, sim='sphere', tmax=1000)
+s.run_simulation(h=.1, sim='sphere', tmax=500)
 s.post_process()
-s.save_results('data/random_h01.mat')
+s.save_results('data/random_sphere.mat')
 
-s.run_simulation(h=.01, sim='sphere', tmax=1000)
+s.run_simulation(h=.1, sim='lie-poisson', tmax=500)
 s.post_process()
-s.save_results('data/random_h001.mat')
+s.save_results('data/random_lp.mat')
