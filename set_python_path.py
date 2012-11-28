26cbc31f556227cc3ad25ba788ba69131f57eea7
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2012-11-28 09:18:45 +0000

    Update

--- set_python_path.py
+++ set_python_path.py
@@ -1,5 +1,5 @@
-#!/bin/sh
-
+# From this directory, run with "source set_python_path"
+# 
 # Add the base directory of this project to the python search path
 
 export PYTHONPATH="$PYTHONPATH:`pwd`"
