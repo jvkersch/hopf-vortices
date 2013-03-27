336de3441b1c59a6dd79dde52bbe1234f70b3c51
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2013-03-27 12:16:49 +0000

    Removed simulations directory

--- simulations/generic/generic_simulation_long.py
+++
@@ -1,8 +0,0 @@
-from simulation import Simulation
-
-# Sphere integrator
-s = Simulation()
-s.load_initial_conditions('random_initial_conditions.mat')
-s.run_simulation(h=.1, sim='sphere', tmax=10000)
-s.post_process()
-s.save_results('data/random_long_h01.mat')
