caa33f5eb637bee0e3ef97c63f640682e1084c71
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2013-04-02 18:50:33 +0100

    Slimmed down version of collapse simulation

---
+++ worksheets/collapse3/collapse_simulation.py
@@ -0,0 +1,48 @@
+from hopf.simulation import Simulation
+
+# Sphere integrator
+s = Simulation()
+s.load_initial_conditions('collapse3.mat')
+
+# No regularization
+#s.sigma = 0.0
+#s.run_simulation(tmax=15, numpoints=75, sim='sphere')
+#s.post_process()
+#s.save_results('data/collapse3_sphere_sigma00_sim.mat')
+
+# Medium regularization
+s.sigma = 0.10
+s.run_simulation(tmax=15, numpoints=75, sim='sphere')
+s.post_process()
+s.save_results('data/collapse3_sphere_sigma10_sim.mat')
+
+# Large regularization
+s.sigma = 0.25
+s.run_simulation(tmax=15, numpoints=75, sim='sphere')
+s.post_process()
+s.save_results('data/collapse3_sphere_sigma25_sim.mat')
+
+# Runge-Kutta, medium regularization
+s.sigma = 0.10
+s.run_simulation(tmax=15, numpoints=75, sim='rk4')
+s.post_process()
+s.save_results('data/collapse3_rk4_sigma10_sim.mat')
+
+# Lie-Poisson, medium regularization
+s.sigma = 0.10
+s.run_simulation(tmax=15, numpoints=75, sim='lie-poisson')
+s.post_process()
+s.save_results('data/collapse3_lp_sigma10_sim.mat')
+
+# Midpoint integration, medium regularization
+s.sigma = 0.10
+s.run_simulation(tmax=15, numpoints=75, sim='midpoint')
+s.post_process()
+s.save_results('data/collapse3_mp_sigma10_sim.mat')
+
+
+# Output for plotting 
+s.sigma = 0.10
+s.run_simulation(tmax=10, numpoints=1000, h=0.01, sim='sphere')
+s.post_process()
+s.save_results('data/collapse3_sphere_sigma10_hi_sim.mat')
---
+++ worksheets/collapse3/collapse_simulation_nosigma.py
@@ -0,0 +1,29 @@
+from hopf.simulation import Simulation
+
+# Sphere integrator
+s = Simulation()
+s.load_initial_conditions('collapse3.mat')
+
+# No regularization
+s.sigma = 0.0
+s.run_simulation(tmax=15, numpoints=75, sim='sphere')
+s.post_process()
+s.save_results('data/collapse3_sphere_sigma00_sim.mat')
+
+# Runge-Kutta, no regularization
+s.sigma = 0.0
+s.run_simulation(tmax=15, numpoints=75, sim='rk4')
+s.post_process()
+s.save_results('data/collapse3_rk4_sigma00_sim.mat')
+
+# Midpoint, no regularization
+s.sigma = 0.0
+s.run_simulation(tmax=15, numpoints=75, sim='midpoint')
+s.post_process()
+s.save_results('data/collapse3_mp_sigma00_sim.mat')
+
+# Lie-Poisson, no regularization
+s.sigma = 0.0
+s.run_simulation(tmax=15, numpoints=75, sim='lie-poisson')
+s.post_process()
+s.save_results('data/collapse3_lp_sigma00_sim.mat')
