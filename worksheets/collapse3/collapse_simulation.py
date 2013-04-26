a893bc94e61e5f3916313281b53d7f094f5ad989
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2013-04-26 12:20:22 +0100

    Switched to implicit midpoint method

--- worksheets/collapse3/collapse_simulation.py
+++ worksheets/collapse3/collapse_simulation.py
@@ -5,20 +5,20 @@ s = Simulation()
 s.load_initial_conditions('collapse3.mat')
 
 # No regularization
-#s.sigma = 0.0
-#s.run_simulation(tmax=15, numpoints=75, sim='sphere')
-#s.post_process()
-#s.save_results('data/collapse3_sphere_sigma00_sim.mat')
+s.sigma = 0.0
+s.run_simulation(tmax=15, numpoints=75, sim='sphere-midpoint-momentum')
+s.post_process()
+s.save_results('data/collapse3_sphere_sigma00_sim.mat')
 
 # Medium regularization
 s.sigma = 0.10
-s.run_simulation(tmax=15, numpoints=75, sim='sphere')
+s.run_simulation(tmax=15, numpoints=75, sim='sphere-midpoint-momentum')
 s.post_process()
 s.save_results('data/collapse3_sphere_sigma10_sim.mat')
 
 # Large regularization
 s.sigma = 0.25
-s.run_simulation(tmax=15, numpoints=75, sim='sphere')
+s.run_simulation(tmax=15, numpoints=75, sim='sphere-midpoint-momentum')
 s.post_process()
 s.save_results('data/collapse3_sphere_sigma25_sim.mat')
 
@@ -43,6 +43,7 @@ s.save_results('data/collapse3_mp_sigma10_sim.mat')
 
 # Output for plotting 
 s.sigma = 0.10
-s.run_simulation(tmax=10, numpoints=1000, h=0.01, sim='sphere')
+s.run_simulation(tmax=10, numpoints=1000, h=0.01, 
+                 sim='sphere-midpoint-momentum')
 s.post_process()
 s.save_results('data/collapse3_sphere_sigma10_hi_sim.mat')
--- worksheets/collapse3/collapse_simulation_nosigma.py
+++ worksheets/collapse3/collapse_simulation_nosigma.py
@@ -6,7 +6,7 @@ s.load_initial_conditions('collapse3.mat')
 
 # No regularization
 s.sigma = 0.0
-s.run_simulation(tmax=15, numpoints=75, sim='sphere')
+s.run_simulation(tmax=15, numpoints=75, sim='sphere-midpoint-momentum')
 s.post_process()
 s.save_results('data/collapse3_sphere_sigma00_sim.mat')
 
