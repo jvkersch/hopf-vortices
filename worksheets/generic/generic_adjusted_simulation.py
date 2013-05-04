05b5ab3b14be27c1f4554cbfe18ae92767931b6c
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2013-05-04 23:07:38 +0100

    Record adjusted timing info

--- worksheets/generic/generic_adjusted_simulation.py
+++ worksheets/generic/generic_adjusted_simulation.py
@@ -1,32 +1,23 @@
+import json
+import timeit
+
+setup_stmt = """
 from hopf.simulation import Simulation
-
 s = Simulation()
 s.load_initial_conditions('random40.mat')
-s.sigma = 0.10
-s.run_simulation(h = 0.02, tmax=2000, numpoints=1000, sim='sphere-midpoint')
-s.post_process()
-s.save_results('data/generic40_T200_sphere.mat')
 
-s = Simulation()
-s.load_initial_conditions('random40.mat')
-s.sigma = 0.10
-s.run_simulation(h = 0.025, tmax=2000, numpoints=1000, sim='rk4')
-s.post_process()
-s.save_results('data/generic40_T200_rk4.mat')
+"""
 
-s = Simulation()
-s.load_initial_conditions('random40.mat')
-s.sigma = 0.10
-s.run_simulation(h = 0.25, tmax=2000, numpoints=1000, sim='midpoint')
-s.post_process()
-s.save_results('data/generic40_T200_mp.mat')
+times = {}
 
-s = Simulation()
-s.load_initial_conditions('random40.mat')
-s.sigma = 0.10
-s.run_simulation(h = 0.0625, tmax=2000, numpoints=1000, sim='lie-poisson')
-s.post_process()
-s.save_results('data/generic40_T200_lp.mat')
+for h, integrator in [(.03, 'rk4'), (.05, 'sphere-midpoint'), 
+                      (0.3, 'midpoint'), (0.1, 'lie-poisson')]:
+    # Statement to time (run simulation but do not save results)
+    stmt="s.run_simulation(h=%f, tmax=10, sim=\'%s\'); s.post_process(); s.save_results('data/generic_%s.mat')" % (h, integrator, integrator)
+    t = timeit.Timer(stmt=stmt, setup=setup_stmt)
+    times[integrator] = t.timeit(number=1)
 
+
+json.dump(times, open('data/timings.txt', 'w'))
 
 
