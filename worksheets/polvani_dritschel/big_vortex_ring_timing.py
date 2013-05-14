8f6d20bd56160c72c9a0166c6da7144a9175c029
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2013-05-14 08:57:11 -0400

    Do several runs to improve accuracy of timings

--- worksheets/polvani_dritschel/big_vortex_ring_timing.py
+++ worksheets/polvani_dritschel/big_vortex_ring_timing.py
@@ -4,6 +4,7 @@ from hopf.util.matlab_io import save_variables
 from numpy import logspace
 
 
+N = 10
 
 setup_stmt = """
 from hopf.simulation import Simulation
@@ -12,14 +13,16 @@ s.load_initial_conditions('equatorial_ring_40.mat')
 s.sigma = 0.10
 
 """
+integrators = ['rk4', 'sphere-midpoint', 'midpoint', 'lie-poisson']
+times = {integrator: 0.0 for integrator in integrators}
 
-times = {}
-
-for integrator in ['rk4', 'sphere-midpoint', 'midpoint', 'lie-poisson']:
-    # Statement to time (run simulation but do not save results)
-    stmt="s.run_simulation(h=0.1, tmax=50, sim=\'%s\')" % integrator
-    t = timeit.Timer(stmt=stmt, setup=setup_stmt)
-    times[integrator] = t.timeit(number=1)
+for _ in xrange(0, N):
+    # Do N runs with timings
+    for integrator in integrators:
+        # Statement to time (run simulation but do not save results)
+        stmt="s.run_simulation(h=0.1, tmax=50, sim=\'%s\')" % integrator
+        t = timeit.Timer(stmt=stmt, setup=setup_stmt)
+        times[integrator] += t.timeit(number=1)
 
 json.dump(times, open('data/timings.txt', 'w'))
 
