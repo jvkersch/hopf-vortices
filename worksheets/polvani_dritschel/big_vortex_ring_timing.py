9bd113b174efbee6ad747d58d7ede6529a51069c
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2013-05-14 21:54:13 -0400

    Record individual changes

--- worksheets/polvani_dritschel/big_vortex_ring_timing.py
+++ worksheets/polvani_dritschel/big_vortex_ring_timing.py
@@ -14,7 +14,7 @@ s.sigma = 0.10
 
 """
 integrators = ['rk4', 'sphere-midpoint', 'midpoint', 'lie-poisson']
-times = {integrator: 0.0 for integrator in integrators}
+times = {integrator: [] for integrator in integrators}
 
 for _ in xrange(0, N):
     # Do N runs with timings
@@ -22,7 +22,7 @@ for _ in xrange(0, N):
         # Statement to time (run simulation but do not save results)
         stmt="s.run_simulation(h=0.1, tmax=50, sim=\'%s\')" % integrator
         t = timeit.Timer(stmt=stmt, setup=setup_stmt)
-        times[integrator] += t.timeit(number=1)
+        times[integrator].append( t.timeit(number=1) )
 
 json.dump(times, open('data/timings.txt', 'w'))
 
